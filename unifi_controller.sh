37759af856a51dee21818448e941958d9160dbeb
Maxim Kirilov <kir.maxim@gmail.com>
2020-11-21 22:55:02 +0200

    wip

--- unifi_controller.sh
+++ unifi_controller.sh
@@ -3,20 +3,12 @@
 set -x
 
 
-# https://stackoverflow.com/questions/31324981/how-to-access-host-port-from-docker-container
-if [ -f /.dockerenv ]; then
-    export HOST_IP=$(ip route show | awk '/default/ {print $3}')
-else
-    export HOST_IP="127.0.0.1"
-fi
-
-
 function start()
 {
     docker-compose up -d
     docker ps | grep -q unifi-controller
     sleep 10
-    curl --connect-timeout 5 \--max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -kILs --fail http://${HOST_IP}:8080 || exit 1
+    curl --connect-timeout 5 --max-time 10 --retry 5 --retry-delay 0 --retry-max-time 60 -kILs --fail http://${HOST_IP}:8080 || exit 1
     exit $?
 }
 
